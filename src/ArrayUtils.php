bd2ccbee405f8984a359086a28c644dfd89137fe
Robert Snedeker <44886996+CommandString@users.noreply.github.com>
2022-12-05 08:38:34 -0500

    Added trimValues method

--- src/ArrayUtils.php
+++ src/ArrayUtils.php
@@ -23,6 +23,19 @@ class ArrayUtils {
 
         return $toStdClass($array, $toStdClass);
     }
+    
+    public static function trimValues(array $array, string $characters = " \n\r\t\v\x00"): array
+    {
+        foreach ($array as $key => $value) {
+            if (is_array($value)) {
+                $value = self::trimValues($array);   
+            } else {
+                $value = trim($value, $characters);
+            }
+        }
+        
+        return $array;
+    }
 
     public static function getLastItem(array $array): mixed
     {
