f478e51a2430009f43817cf2bbf85b78c898520e
CommandString <bmxmikey2@gmail.com>
2022-12-19 14:41:59 -0500

    Back to the original method

--- src/ArrayUtils.php
+++ src/ArrayUtils.php
@@ -10,27 +10,13 @@ class ArrayUtils {
         $toStdClass = function (array $array, callable $toStdClass) {
             $stdClass = new stdClass();
 
-            $toStdClass = function (\Closure $toStdClass, array $array, array $levels = []) use ($stdClass) {
-                foreach ($array as $key => $value) {
-                    if (is_array($value)) {
-                        $newLevels = array_merge($levels, [$key]);
-                        $toStdClass($toStdClass, $value, $newLevels);
-                        continue;
-                    }
-
-                    $level = &$stdClass;
-                    foreach ($levels as $levelName) {
-                        if (!isset($level->$levelName)) {
-                            $level->$levelName = new stdClass;
-                        }
-
-                        $level = &$level->$levelName;
-                    }
-                    $level->$key = $value;
+            foreach ($array as $key => $value) {
+                if (is_array($value)) {
+                    $value = $toStdClass($value, $toStdClass);
                 }
-            };
 
-            $toStdClass($toStdClass, $array);
+                $stdClass->$key = $value;
+            }
 
             return $stdClass;
         };
