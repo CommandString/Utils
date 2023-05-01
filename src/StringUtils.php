8700a7b31d540692b8de67643bc3496966096852
commandstring <bmxmikey2@gmail.com>
2023-05-01 11:01:56 -0400

    Fixed exception not being thrown

--- src/StringUtils.php
+++ src/StringUtils.php
@@ -15,13 +15,13 @@ class StringUtils
         $startPos = stripos($string, $start);
         $endPos = strripos($string, $end);
 
+        if ($startPos === false || $endPos === false) {
+            throw new LogicException("Unable to find the start and/or the end position!");
+        }
+
         if (!$include_start_end_with_response) {
             $startPos += strlen($start);
             $endPos -= strlen($end);
-        }
-
-        if ($startPos === false || $endPos === false) {
-            throw new LogicException("Unable to find the start and/or the end position!");
         }
 
         return substr($string, $startPos, ($endPos - $startPos) + 1);
